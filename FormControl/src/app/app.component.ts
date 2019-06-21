a6133bea3b4563649032b31ec0485feabf64befd
Nkosinathi Mtshali ka Hlophe <tcfworld72@gmail.com>
2019-06-21 16:13:55 +0200

    Form Controls Functions

---
+++ FormControl/src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validator, Validators } from '@angular/forms';
+import { TouchSequence } from 'selenium-webdriver';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FormControl';
+
+  myPeople = [];
+  myFormz;
+  name: string;
+  surname: string;
+  age: string;
+  email: string;
+  phoneNumbers: string;
+
+  constructor(public formBuilder: FormBuilder) {
+    this.myFormz = formBuilder.group({
+      name : ['', Validators.required],
+      surname : ['', Validators.required],
+      age : ['', Validators.maxLength(2)],
+      email : ['', Validators.email],
+      phoneNumbers : ['', Validators.maxLength(10)]
+    });
+  }
+
+  addUser() {
+    this.myPeople.push({
+      name: this.myFormz.value.name,
+      surname: this.myFormz.value.surname,
+      age: this.myFormz.value.age,
+      email: this.myFormz.value.email,
+      phoneNumber: this.myFormz.value.phoneNumbers
+    });
+    this.myFormz.value.name = '';
+    this.surname = '';
+    this.age = '';
+    this.email = '';
+    this.phoneNumbers = '';
+  }
+
+}
---
+++ FormControl/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatInputModule, MatButtonModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+export class PizzaPartyAppModule { }
